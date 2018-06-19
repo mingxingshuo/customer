4b1b9a19ea417d0d59f3bcf0ab96fe9364cf779e
unknown <weblixin@163.com>
2018-06-19 15:01:29 +0800

    客服消息

--- project/routes/kefu.js
+++ project/routes/kefu.js
@@ -3,7 +3,7 @@ const router = require('koa-router')()
 router.prefix('/kefu');
 
 router.all('/*',async (ctx,next)=>{
-	this.redirect('/message_1/index');
+	await ctx.render('message_1/index')
 })
 
 module.exports = router
