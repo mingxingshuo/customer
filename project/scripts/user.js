bceaae12022a1a990818bf7945658e824db1cc8e
efdssdf <efdssdf@gmail.com>
2018-06-20 14:54:11 +0800

    'test'

--- project/scripts/user.js
+++ project/scripts/user.js
@@ -4,8 +4,12 @@ var FuUserModel = require('../model/FuUser');
 var async = require('async');
 var schedule = require("node-schedule");
 
-FuUserModel.deleteOne({'openid':'oD4KE1IxZ33wN_cRakxCZHCgcn8s'})
-FuUserModel.deleteOne({'openid':'oD4KE1G7wwq8LSU1TZ5gRD3IPfxQ'})
+FuUserModel.deleteOne({'openid':'oD4KE1IxZ33wN_cRakxCZHCgcn8s'},function (err,data) {
+    console.log(data,'------------------data1')
+})
+FuUserModel.deleteOne({'openid':'oD4KE1G7wwq8LSU1TZ5gRD3IPfxQ'},function (err,data) {
+    console.log(data,'------------------data2')
+})
 
 function get_message() {
     MessageModel.find({task: true}, function (err, messages) {
